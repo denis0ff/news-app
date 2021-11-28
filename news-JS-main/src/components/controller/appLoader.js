f7f5069050a4ac2060ef361182859776a3ce6e21
Denis Popov <popovdw94@gmail.com>
2021-11-28 14:27:24 +0300

    init: start migration-newip-to-ts task

---
+++ news-JS-main/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: '7f12bc8d8e3745aa8c82f86598e5c437', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
