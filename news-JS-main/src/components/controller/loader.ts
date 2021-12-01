88b01d54bded5149d6d4db14771c6b763b05cd25
Denis Popov <popovdw94@gmail.com>
2021-12-01 22:32:59 +0300

    fix: set types in loader.ts file

--- news-JS-main/src/components/controller/loader.ts
+++ news-JS-main/src/components/controller/loader.ts
@@ -1,19 +1,25 @@
+interface IOptions {
+  [key: string]: string;
+}
+
 class Loader {
-  constructor(baseLink: string, options: { apiKey: string }) {
+  baseLink: string;
+  options: IOptions;
+  constructor(baseLink: string, options: IOptions) {
     this.baseLink = baseLink;
     this.options = options;
   }
 
   getResp(
-    { endpoint, options = {} },
-    callback = () => {
+    { endpoint, options = {} }: { endpoint: string; options?: IOptions | {} },
+    callback = (): void => {
       console.error('No callback for GET response');
     }
-  ) {
+  ): void {
     this.load('GET', endpoint, callback, options);
   }
 
-  errorHandler(res) {
+  errorHandler(res: Response) {
     if (!res.ok) {
       if (res.status === 401 || res.status === 404)
         console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +29,7 @@ class Loader {
     return res;
   }
 
-  makeUrl(options, endpoint) {
+  makeUrl(options: IOptions | {}, endpoint: string): string {
     const urlOptions = { ...this.options, ...options };
     let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,12 +40,12 @@ class Loader {
     return url.slice(0, -1);
   }
 
-  load(method, endpoint, callback, options = {}) {
+  load(method: string, endpoint: string, callback: (data?: object) => void, options: IOptions | {}) {
     fetch(this.makeUrl(options, endpoint), { method })
       .then(this.errorHandler)
-      .then((res) => res.json())
+      .then((res) => res.json() as object)
       .then((data) => callback(data))
-      .catch((err) => console.error(err));
+      .catch((err: PromiseRejectedResult) => console.error(err));
   }
 }
 
